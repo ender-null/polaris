021487dc7ac0c465be0464a89dfb2305babeafd5
Ender <luksireiku@icloud.com>
2020-10-26 18:57:44 +0100

    Delete credentials

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import { readFileSync } from 'fs';
 import { Bot, Config } from '.';
 
 const bots = [];
@@ -13,18 +14,7 @@ export const stop = (): void => {
   process.exit(1);
 };
 
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyDj_0pcAmf_NiTzZT7B0QEY1tPfrnlnqSY',
-  authDomain: 'polaris-bot.firebaseapp.com',
-  databaseURL: 'https://polaris-bot.firebaseio.com',
-  projectId: 'polaris-bot',
-  storageBucket: 'polaris-bot.appspot.com',
-  messagingSenderId: '617329433754',
-  appId: '1:617329433754:web:afd6a7ded8b87d19955323',
-};
-
-// Initialize Firebase
+const firebaseConfig = JSON.parse(readFileSync('firebase.json', 'utf-8'));
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 db.ref('configs')
