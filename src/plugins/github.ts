ac47c3d45ab53ae6b4874024f7a5009a77907f7b
Alin Sorin Nedelcu <asnedelcu@icloud.com>
2020-12-16 23:54:46 +0100

    feat: wip github plugin

--- src/plugins/github.ts
+++ src/plugins/github.ts
@@ -8,11 +8,19 @@ export class GitHubPlugin extends PluginBase {
   }
 
   async webhook(url: string, data: string): Promise<void> {
-    logger.info(`github plugin: ${url}`);
-    if (data['head_commit']) {
-      this.bot.sendAdminAlert(`new commit: ${data['head_commit']['message']}`);
+    logger.debug(`github plugin: ${url}`);
+    const cb = JSON.parse(data);
+    const text = `🔨 <a href="${cb.compare}">${cb.commits.length} new commit</a> <b>to ${
+      cb.repository.name
+    }:${cb.ref.replace('refs/heads/', '')}</b>:\n\n<a href="${cb.head_commit.url}">${cb.head_commit.id.slice(
+      0,
+      7,
+    )}</a>: ${cb.head_commit.message} by ${cb.head_commit.committer.name}`;
+
+    if (cb.head_commit) {
+      this.bot.sendAdminAlert(text);
     } else {
-      logger.info(`other`);
+      logger.debug(`other`);
     }
   }
 }
