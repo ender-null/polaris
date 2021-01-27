f296eed535cb3d054a5914f50c17d2733f00d6d2
Alin Sorin Nedelcu <asnedelcu@icloud.com>
2021-01-27 19:52:00 +0100

    webhook

--- src/plugins/github.ts
+++ src/plugins/github.ts
@@ -10,9 +10,14 @@ export class GitHubPlugin extends PluginBase {
 
   async webhook(url: string, data: string): Promise<void> {
     logger.debug(`github plugin: ${url}`);
-    const cb = JSON.parse(data);
+    let cb;
+    try {
+      cb = JSON.parse(data);
+    } catch (e) {
+      logger.error('Invalid JSON object received');
+    }
 
-    if (cb.head_commit) {
+    if (cb && cb.head_commit) {
       const text = `<a href="${cb.compare}">${cb.commits.length} new commit</a> to <b>${
         cb.repository.name
       }:${cb.ref.replace('refs/heads/', '')}</b>\n\n<a href="${cb.head_commit.url}">${cb.head_commit.id.slice(
