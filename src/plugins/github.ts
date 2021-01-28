b035a269708489b540b43b5e664d00bc7c32f6d8
Alin Sorin Nedelcu <asnedelcu@icloud.com>
2021-01-28 10:20:42 +0100

    github actions webhook

--- src/plugins/github.ts
+++ src/plugins/github.ts
@@ -1,7 +1,7 @@
 import { Bot } from '..';
 import { PluginBase } from '../plugin';
 import { Conversation } from '../types';
-import { getTaggedWith, logger } from '../utils';
+import { capitalize, getTaggedWith, logger } from '../utils';
 
 export class GitHubPlugin extends PluginBase {
   constructor(bot: Bot) {
@@ -11,14 +11,22 @@ export class GitHubPlugin extends PluginBase {
   async webhook(url: string, data: any): Promise<void> {
     logger.debug(`github plugin: ${url}`);
 
-    if (data && data.head_commit) {
-      const text = `<a href="${data.compare}">${data.commits.length} new commit</a> to <b>${
-        data.repository.name
-      }:${data.ref.replace('refs/heads/', '')}</b>\n\n<a href="${data.head_commit.url}">${data.head_commit.id.slice(
-        0,
-        7,
-      )}</a>: <i>${data.head_commit.message}</i> by ${data.repository.owner.name}`;
+    if (data) {
+      let text;
       const subs = getTaggedWith(this.bot, `sub:github/${data.repository.name}`);
+      if (data.head_commit) {
+        text = `<a href="${data.compare}">${data.commits.length} new commit</a> to <b>${
+          data.repository.name
+        }:${data.ref.replace('refs/heads/', '')}</b>\n\n<a href="${data.head_commit.url}">${data.head_commit.id.slice(
+          0,
+          7,
+        )}</a>: <i>${data.head_commit.message}</i> by ${data.repository.owner.name}`;
+      }
+      if (data.action) {
+        text = `${capitalize(data.action)} check run <a href="${data.check_run.details_url}">${
+          data.check_run.id
+        }</a> with status: ${data.check_run.status}`;
+      }
       for (const sub of subs) {
         this.bot.sendMessage(new Conversation(sub), text);
       }
