5f7c2760c54133471966428261d3f4ad5e712ab8
Ender <endernull@icloud.com>
2022-01-24 14:51:09 +0100

    bugfixes

--- src/plugins/rae.ts
+++ src/plugins/rae.ts
@@ -1,4 +1,3 @@
-import * as cheerio from 'cheerio';
 import { Bot, Message } from '..';
 import { PluginBase } from '../plugin';
 import { generateCommandHelp, getInput, sendRequest } from '../utils';
