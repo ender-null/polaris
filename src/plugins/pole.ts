54cdf4b03841848023d4d6bc292f4407cdd8ad73
endernull <ender@end.works>
2026-02-19 00:40:55 +0100

    fix poles

--- src/plugins/pole.ts
+++ src/plugins/pole.ts
@@ -122,7 +122,7 @@ export class PolePlugin extends PluginBase {
     if (String(msg.sender.id).startsWith('-100')) {
       return;
     }
-    if (!String(msg.conversation.id).startsWith('-') || msg.conversation.type !== 'group') {
+    if (msg.conversation.type ? msg.conversation.type !== 'group' : !String(msg.conversation.id).startsWith('-')) {
       return this.bot.replyMessage(msg, this.bot.errors.groupOnly);
     }
     if (await hasTag(this.bot, msg.conversation.id, 'nopole')) {
